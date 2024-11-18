2ca1ec000b7a06d13255c49e4e4ec747f10f9885
Ananacik0 <armeniya.02@gmail.com>
2024-11-18 20:19:24 +0300

    add listener function for category list (main.js) in index.hmtl and new style for other element

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
-const a = 45;
-const b = 32;
+const catalog = document.querySelector('#item__category--list');
 
+const openCatalog = (event) => {
+  console.log(event);
+  if (event.target.id === 'item__category--list') {
+    catalog.innerHTML += `
+    <ul class='category__list--active'>
+      <li class='category__list--item'>
+        <a class='category__list--item--link' href=''>fafdafaf</a>
+      </li>
+      <li class='category__list--item'>
+        <a class='category__list--item--link' href=''>sdagasfgafg</a>
+      </li>
+      <li class='category__list--item'>
+        <a class='category__list--item--link' href=''>asgdawgwgwrg</a>
+      </li>
+      <li class='category__list--item'>
+        <a class='category__list--item--link' href=''>wrgwrgwrgw</a>
+      </li>
+      <li class='category__list--item'>
+        <a class='category__list--item--link' href=''>rgwrwr</a>
+      </li>
+    </ul>
+    `;
+  }
+};
 
+const dropCatalog = catalog.addEventListener('click', openCatalog);
