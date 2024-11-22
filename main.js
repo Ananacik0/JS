a73c8baa6d20815fa3286eca9cb9e676b49cbbc7
Ananacik0 <armeniya.02@gmail.com>
2024-11-22 20:24:53 +0300

    new function in main.js for drop menu block, new style for nav block

--- main.js
+++ main.js
@@ -1,28 +1,35 @@
-const catalog = document.querySelector('#item__category--list');
+const catalog = document.querySelector('#item__category--list--noactive');
 
 const openCatalog = (event) => {
   console.log(event);
-  if (event.target.id === 'item__category--list') {
+  if (event.target.id === 'item__category--list--noactive' && event.target.localName === 'li') {
+    event.target.id = 'item__category--list--active';
+    console.log(event);
     catalog.innerHTML += `
-    <ul class='category__list--active'>
+    <ul class='category__list'>
       <li class='category__list--item'>
-        <a class='category__list--item--link' href=''>fafdafaf</a>
+        <a class='category__list--item--link' href=''>Лендинг</a>
       </li>
       <li class='category__list--item'>
-        <a class='category__list--item--link' href=''>sdagasfgafg</a>
+        <a class='category__list--item--link' href=''>Интернет-магазин</a>
       </li>
       <li class='category__list--item'>
-        <a class='category__list--item--link' href=''>asgdawgwgwrg</a>
+        <a class='category__list--item--link' href=''>CRM-система</a>
       </li>
       <li class='category__list--item'>
-        <a class='category__list--item--link' href=''>wrgwrgwrgw</a>
+        <a class='category__list--item--link' href=''>Telegram-Bot</a>
       </li>
       <li class='category__list--item'>
-        <a class='category__list--item--link' href=''>rgwrwr</a>
+        <a class='category__list--item--link' href=''>Web-приложение</a>
       </li>
     </ul>
     `;
-  }
+    console.log(catalog);
+  } else {
+    event.target.id = 'item__category--list--noactive';
+    let dropMenu = document.querySelector('.category__list');
+    dropMenu.remove();
+  };
 };
 
 const dropCatalog = catalog.addEventListener('click', openCatalog);
